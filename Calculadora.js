bdaf769d65a710a96e644d2bf1d021a8f43c13e1
MauricioSA5401 <134406537+MauricioSA5401@users.noreply.github.com>
2024-10-29 07:33:10 -0600

    Update Calculadora.js

--- Calculadora.js
+++ Calculadora.js
@@ -10,7 +10,7 @@ function restar(a, b) {
 function multiplicar(a, b) {
     return a * b;
 }
-// Función para dividir
+// Función dividir
 function dividir(a, b) {
     if (b === 0) {
         throw new Error("No se puede dividir por cero");
